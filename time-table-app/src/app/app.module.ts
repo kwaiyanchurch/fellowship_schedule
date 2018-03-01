d976e52b926fa37aefceb378d922c4ddf66774dd
kwaiyanchurch <info@kwaiyanchurch.org>
2018-03-02 01:09:41 +0800

    fellowship box to 100%

--- time-table-app/src/app/app.module.ts
+++ time-table-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FellowshipBoxComponent } from './fellowship-box/fellowship-box.component';
@@ -11,7 +12,8 @@ import { FellowshipBoxComponent } from './fellowship-box/fellowship-box.componen
     FellowshipBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
